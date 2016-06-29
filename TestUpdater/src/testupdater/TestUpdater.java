11246df4d1e1b1d155ca9764c2dcaf8b2ea7efaf
H9073 <kivisto.vesku@gmail.com>
2016-06-29 15:28:03 +0300

    stuff

--- TestUpdater/src/testupdater/TestUpdater.java
+++ TestUpdater/src/testupdater/TestUpdater.java
@@ -1,36 +1,49 @@
 package testupdater;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TestUpdater {
 
     public static void main(String[] args) throws IOException {
-        //kill();
-        //getUpdate();
+        kill();
+        get();
         start();
     }
-    
+    /*
     public static void getUpdate() throws IOException{
         Process process = null;
         String cmd = "wget -P /home/h3694/UpdateTest/ "
                 + "https://github.com/Saukonoja/literate-guide/releases/download/0.1.2/dist.tar.gz;"
-                + " rm /home/h3694/UpdateTest/UpdateTest.jar;"
-                + " cd /home/h3694/UpdateTest;"
+                + " rm /home/h9073/UpdateTest/UpdateTest.jar;"
+                + " cd /home/h9073/UpdateTest;"
                 + " tar -vxzf dist.tar.gz;";
         process = Runtime.getRuntime().exec(cmd);
     }
-    
+    */
     public static void kill() throws IOException{
-        String cmd2 = "/home/h3694/kill.sh";
+        String cmd2 = "/home/h9073/kill.sh";
         Process process2 = new ProcessBuilder(cmd2).start();
     }
     
-    public static void start() throws IOException{
-        System.out.println("ennen");
-        String cmd3 = "/home/h3694/wget.sh";  
+    public static void get() throws IOException{
+        Process p = Runtime.getRuntime().exec("wget -P /home/h9073/Documents/Repos/literate-guide/UpdateTest/ https://github.com/Saukonoja/literate-guide/releases/download/0.1.2/dist.tar.gz");
+        try {
+            p.waitFor();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(TestUpdater.class.getName()).log(Level.SEVERE, null, ex);
+        }
+   }
+    
+    public static void start() throws IOException {
+        String cmd3 = "/home/h9073/start.sh";
         Process process3 = new ProcessBuilder(cmd3).start();
-        System.out.println("jälkeen");
     }
+    
+    
     
     
 }
