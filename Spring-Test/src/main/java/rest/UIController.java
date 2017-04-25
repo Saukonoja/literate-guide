45cce77dbab9d55f325fe254c135724318496e06
Ubuntu <ubuntu@ip-172-31-55-131.ec2.internal>
2017-04-25 20:37:50 +0000

    fix typo and minor other fixes

--- Spring-Test/src/main/java/rest/UIController.java
+++ Spring-Test/src/main/java/rest/UIController.java
@@ -1,9 +1,12 @@
-import org.springframework.web.bind.annotation.RequestBody;
+package rest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
 
 @Controller
 public class UIController{
@@ -11,20 +14,21 @@ public class UIController{
 	
 	@GetMapping("/ui")
     public String uiGet(Model model) {
-        ResultSet result;
+        /*ResultSet result;
         result = cc.selectCassie("SELECT * FROM users");
         Row row = result.one();
         model.addAttribute("user", 
             new User(row.getString("username"),
                     row.getString("firstname"),
                     row.getString("lastname"),
-                    row.getInt("age")));
+                    row.getInt("age")));*/
+	model.addAttribute("user", new User());
         return "user";
     }
 
     @PostMapping("/ui")
-    public String userSubmit(@ModelAttribute User u) {
-        cc.insertCassie("users", u.getUsername(), u.getFirstname(), u.getLastname(), u.getAge());
+    public String userSubmit(@ModelAttribute User user) {
+        //cc.insertCassie("users", u.getUsername(), u.getFirstname(), u.getLastname(), u.getAge());
         return "result";
     }
 }
--- Spring-Test/src/main/java/rest/UserController.java
+++ Spring-Test/src/main/java/rest/UserController.java
@@ -1,5 +1,6 @@
 package rest;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
